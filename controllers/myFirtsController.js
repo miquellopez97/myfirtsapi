f959ca9b57dd6f90f4d3c4e03299699df0949136
miquellopez97 <58914599+miquellopez97@users.noreply.github.com>
2021-10-05 19:33:36 +0200

    new

--- controllers/myFirtsController.js
+++ controllers/myFirtsController.js
@@ -3,3 +3,15 @@ module.exports.helloWorld = (req, res) => {
     const responseStatus = 200;
     res.status(responseStatus).send(responseObj);
 }
+
+module.exports.alumnos = (req, res) => {
+    const arrAlumn = ["Miquel", "Eric", "Alba", "Albert", "Alejandro", "Biel", "Dani", "Ivar", "Jordi", "Marcos"];
+    const arrlenght = arrAlumn.length;
+    const jsonResponse = {
+        alumnosArr: arrAlumn,
+        alumnesLenght: arrlenght,
+    }
+    const responseStatus = 200;
+
+    res.status(responseStatus).send(jsonResponse);
+}
--- index.js
+++ index.js
@@ -5,7 +5,8 @@ require('dotenv');
 
 const myFirtsController = require('./controllers/myFirtsController');
 
-app.get('/', myFirtsController.helloWorld);
+
+app.get('/', myFirtsController.alumnos);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('My firts API running!')
